7d5c403557b7e8a0ca1fbc044c1ee68562d1ee03
Wouter Wijsman <wwijsman@live.nl>
2022-01-07 02:37:48 +0100

    Fail if linking psp-pkgconf fails

--- scripts/002-psp-pkg-config.sh
+++ scripts/002-psp-pkg-config.sh
@@ -3,5 +3,5 @@
 mkdir -p "${PSPDEV}/bin"
 install -m755 ../patches/psp-pkg-config "${PSPDEV}/bin" || { exit 1; }
 cd "${PSPDEV}/bin"
-ln -sf "psp-pkg-config" "psp-pkgconf"
+ln -sf "psp-pkg-config" "psp-pkgconf" || { exit 1; }
 echo "psp-pkg-config installation finished"
