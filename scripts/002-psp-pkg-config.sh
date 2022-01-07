0f55070bc1ce78559b1bb3be832e7636b9097b58
Wouter Wijsman <wwijsman@live.nl>
2022-01-07 02:33:55 +0100

    Don't use -r flag when linking psp-pkgconf
    Alpine Linux does not support it

--- scripts/002-psp-pkg-config.sh
+++ scripts/002-psp-pkg-config.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+WORKDIR="${PWD}"
 
 mkdir -p "${PSPDEV}/bin"
 install -m755 ../patches/psp-pkg-config "${PSPDEV}/bin" || { exit 1; }
-ln -srf "${PSPDEV}/bin/psp-pkg-config" "${PSPDEV}/bin/psp-pkgconf"
+cd "${PSPDEV}/bin"
+ln -sf "psp-pkg-config" "psp-pkgconf"
 echo "psp-pkg-config installation finished"
-
