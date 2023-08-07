8ce9f48d47ea6643209821de90b50fbd80cbc745
diamant3 <diamant3@proton.me>
2023-08-08 00:17:47 +0800

    fix gpgme-config issue in debian
    fix for gpgme-config debian according to https://github.com/pspdev/pspdev/issues/44

--- depends/check-dependencies.sh
+++ depends/check-dependencies.sh
@@ -66,7 +66,8 @@ check_program   automake
 if [ "${OSVER:0:5}" != MINGW ]; then
 check_program   python3
 check_program   pip3
-check_program   gpgme-config
+check_program   gpgrt-config
+# check_program   gpgme-config
 check_header    openssl             openssl/crypto.h openssl/include/openssl/crypto.h
 check_header    libarchive          archive.h libarchive/include/archive.h
 fi
